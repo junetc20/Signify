662a09011cbec1783aa7e400f16455c2a3067cb6
junetc20 <june.caldwell.2020@uni.strath.ac.uk>
2021-07-19 22:54:24 +0100

    toast updates

--- app/src/main/java/com/example/signify/SignUp.java
+++ app/src/main/java/com/example/signify/SignUp.java
@@ -63,15 +63,21 @@ public class SignUp extends AppCompatActivity{
                         if (checkEmail == true) {
                             Boolean insert = db.insert(newFName, newLName, newEmail, newPassword);
                             if (insert == true) {
-                                Toast.makeText(SignUp.this, "You have registered successfully.", Toast.LENGTH_LONG).show();
+                                Toast.makeText(SignUp.this, "You have registered successfully.", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUp.this, Home.class));
                             }
-                            else {
-                                Toast.makeText(SignUp.this, "This email already exists in this system. Please try logging in.", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(SignUp.this, "This email already exists in this system. Please try logging in.", Toast.LENGTH_SHORT).show();
+                            inputFirstName.setText(null);
+                            inputLastName.setText(null);
+                            inputEmail.setText(null);
+                            createPassword.setText(null);
+                            reEnterPassword.setText(null);
                         }
                     }
-                    }
                     else {
-                        Toast.makeText(SignUp.this, "Passwords do not match. Please try again.", Toast.LENGTH_LONG).show();
+                        Toast.makeText(SignUp.this, "Passwords do not match. Please try again.", Toast.LENGTH_SHORT).show();
                         createPassword.setText(null);
                         reEnterPassword.setText(null);
                     }
