9565635275e6d1047ba7a7194551d016310e6aff
junetc20 <78424119+junetc20@users.noreply.github.com>
2021-08-10 22:20:12 +0100

    updates to home for user guide

--- app/src/main/java/com/example/signify/Home.java
+++ app/src/main/java/com/example/signify/Home.java
@@ -31,6 +31,12 @@ public class Home extends AppCompatActivity {
     Button level3Button;
     Button level4Button;
     TextView firstName;
+    ImageView howTo1;
+    ImageView howTo2;
+    Button nextButtonHelp;
+    Button doneButtonHelp;
+    ImageView exitButtonHelp;
+
 
     @SuppressLint({"ResourceAsColor", "Range"})
     @Override
@@ -50,6 +56,11 @@ public class Home extends AppCompatActivity {
         level3Button = findViewById(R.id.level3Button);
         level4Button = findViewById(R.id.level4Button);
         firstName = findViewById(R.id.textView4);
+        howTo1 = findViewById(R.id.howTo1);
+        howTo2 = findViewById(R.id.howTo2);
+        nextButtonHelp = findViewById(R.id.nextButtonHelp);
+        doneButtonHelp = findViewById(R.id.doneButtonHelp);
+        exitButtonHelp = findViewById(R.id.exitButton20);
 /*
         if (level1Button.getText().toString().equals("REVISIT")) {
             level2Button.setClickable(true);
@@ -68,7 +79,6 @@ public class Home extends AppCompatActivity {
             level4Button.setText(R.string.start);
             level4Button.setBackgroundColor(R.color.BSL_blue);
         }
-
  */
         if(cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
@@ -78,6 +88,58 @@ public class Home extends AppCompatActivity {
         else{
             Toast.makeText(getApplicationContext(), "NO DATA", Toast.LENGTH_LONG).show();
         }
+
+        /**
+         * Set the view from clicking exitButtonHelp.
+         * @param v the onClickListener View.
+         */
+        exitButtonHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                howTo1.setVisibility(View.GONE);
+                howTo2.setVisibility(View.GONE);
+                nextButtonHelp.setVisibility(View.GONE);
+                doneButtonHelp.setVisibility(View.GONE);
+                exitButtonHelp.setVisibility(View.GONE);
+                level1Button.setVisibility(View.VISIBLE);
+                level2Button.setVisibility(View.VISIBLE);
+                level3Button.setVisibility(View.VISIBLE);
+                level4Button.setVisibility(View.VISIBLE);
+            }
+        });
+
+        /**
+         * Set the view from clicking nextButtonHelp.
+         * @param v the onClickListener View.
+         */
+        nextButtonHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                howTo1.setVisibility(View.GONE);
+                howTo2.setVisibility(View.VISIBLE);
+                nextButtonHelp.setVisibility(View.GONE);
+                doneButtonHelp.setVisibility(View.VISIBLE);
+            }
+        });
+
+        /**
+         * Set the view from clicking doneButtonHelp.
+         * @param v the onClickListener View.
+         */
+        doneButtonHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                howTo1.setVisibility(View.GONE);
+                howTo2.setVisibility(View.GONE);
+                nextButtonHelp.setVisibility(View.GONE);
+                doneButtonHelp.setVisibility(View.GONE);
+                exitButtonHelp.setVisibility(View.GONE);
+                level1Button.setVisibility(View.VISIBLE);
+                level2Button.setVisibility(View.VISIBLE);
+                level3Button.setVisibility(View.VISIBLE);
+                level4Button.setVisibility(View.VISIBLE);
+            }
+        });
 
         /**
          * Set the view from clicking userAccount.
