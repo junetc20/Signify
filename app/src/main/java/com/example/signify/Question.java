825b7668556f9ab6e1c854ad3bc499c629e28f30
junetc20 <78424119+junetc20@users.noreply.github.com>
2021-08-13 23:31:11 +0100

    javadoc update

--- app/src/main/java/com/example/signify/Question.java
+++ app/src/main/java/com/example/signify/Question.java
@@ -1,18 +1,37 @@
 package com.example.signify;
 
+/**
+ * A class used to store methods for Quiz2, Quiz3 and Quiz4.
+ *
+ * @author June Caldwell
+ * @version 0.1 (01.08.21)
+ */
+
 public class Question {
+
     private int answerResId;
     private boolean answerTrue;
 
+    /**
+     * Constructor for objects of class Question.
+     */
     public Question(int answerResId, boolean answerTrue) {
         this.answerResId = answerResId;
         this.answerTrue = answerTrue;
     }
 
+    /**
+     * Get the int value of answerResId.
+     * @return int answerResId.
+     */
     public int getAnswerResId() {
         return answerResId;
     }
 
+    /**
+     * Get the boolean value of isAnswerTrue.
+     * @return boolean isAnswerTrue.
+     */
     public boolean isAnswerTrue() {
         return answerTrue;
     }
